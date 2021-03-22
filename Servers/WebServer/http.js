be7771c7bd3ec00ba55dcec0595fad4938545e40
Daniel741312 <syc198@qq.com>
2021-03-23 07:51:59 +0800

    nodejs增加了异常处理；修改了README

--- Servers/WebServer/http.js
+++ Servers/WebServer/http.js
@@ -6,7 +6,7 @@
 const fs = require("fs");
 const http = require("http");
 
-var server = http.createServer();
+const server = http.createServer();
 
 server.listen(9527, function () {
     console.log("请访问localhost:9527");
@@ -18,25 +18,40 @@ server.on("request", function (request, response) {
     if (request.url == "/") {
         /*如果请求的是根目录，则将主页发送给客户端*/
         fs.readFile("./index.html", "utf8", function (err, data) {
+            if(err){
+                throw err;
+            }
             response.end(data);
         });
     } else if (request.url == "/getAllTrashesInfo") {
         /*如果发送过来的是ajax请求，则将发送文件allTrashesInfo.json给客户端*/
         fs.readFile("../allTrashesInfo.json", "utf-8", function (err, data) {
+            if(err){
+                throw err;
+            }
             response.end(data);
         });
     } else if (request.url == "/favicon.ico") {
         fs.readFile("./img/favicon.ico", function (err, data) {
+            if(err){
+                throw err;
+            }
             response.end(data);
         })
 
     } else if (request.url == "/map.js") {
         fs.readFile("./map.js", "utf-8", function (err, data) {
+            if(err){
+                throw err
+            }
             response.end(data);
         });
     } else {
         /*如果请求不同的垃圾桶Icon，将对应的图片发给前端*/
         fs.readFile("./img" + request.url, "binary", function (err, data) {
+            if(err){
+                throw err;
+            }
             response.write(data, "binary");
             response.end(data);
         });

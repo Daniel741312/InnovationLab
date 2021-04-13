61e6242415c797d7dc157e12d7f92b8e5bcc4c42
Daniel741312 <syc198@qq.com>
2021-04-13 11:27:33 +0800

    a

--- RaspberryPi/src/photosensitive_camera.cpp
+++ RaspberryPi/src/photosensitive_camera.cpp
@@ -1,5 +1,8 @@
 #include "photosensitive_camera.h"
 
+/*我的光敏电阻模块坏掉了，临时用键盘上的a键触发*/
+#define HIT_A_TO_TRIGER
+
 int waitForMyPic(void){
 	if(wiringPiSetup()==-1){
 		printf("setup wirginPi failed!\n");
@@ -8,33 +11,45 @@ int waitForMyPic(void){
 
 	pinMode(DO,INPUT);
 
+#ifndef HIT_A_TO_TRIGER
 	while(1){
 		/*block here,wait for DO==1*/
 		while(digitalRead(DO)!=1);
 		sleep(1);
-		/*after 1s,if DO is still 1,break to fork and take a picture*/
+		/*after 1s,if DO is still 1,break to fork a child process and take a picture*/
 		if(digitalRead(DO)==1)
 			break;
 	}
+#else
+	while(1){
+		char triger='\0';
+		scanf("%c",&triger);
 
-	int ret=fork();
-	if(ret<0){
-		perror("fork error");
-		return -1;
+		if(triger=='a')
+			break;
+		else
+			continue;
 	}
+#endif
 
-	/*child process*/
-	if(ret==0){
-		if(execl("/usr/bin/raspistill","raspistill","-o","garbage.jpg","-t","100",NULL)<0){
-			perror("execl error");
-			exit(0);
+		int ret=fork();
+		if(ret<0){
+			perror("fork error");
+			return -1;
 		}
-	}else{
-		/*parent process:block here,wait for child process exit*/
-		wait(NULL);
-		printf("Child process exit,he should take a picture named 'garbage.jpg'\n");
-	}
 
-	return 0;
-}
+		/*child process*/
+		if(ret==0){
+			if(execl("/usr/bin/raspistill","raspistill","-o","garbage.jpg","-t","100",NULL)<0){
+				perror("execl error");
+				exit(0);
+			}
+		}else{
+			/*parent process:block here,wait for child process exit*/
+			wait(NULL);
+			printf("Child process exit,he should take a picture named 'garbage.jpg'\n");
+		}
+
+		return 0;
+	}
 
